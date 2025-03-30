7097bc53dd0e6fc57e30cedcd54ba5a2f48bf6ac
William Bachman <billy@hometownlabs.io>
2025-03-30 01:15:03 -0400

    BB2

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ import 'mapboxgl-legend/dist/style.css';
 import metfile from '../data/metrics.json';
 
 // load image fileSize
-import CFClogo from '../src/assets/CFC_logo.png';
-import UD4Hlogo from '../src/assets/logo2.png';
-import RedHatch from '../src/assets/red_thin.png';
-import HatchLeg from '../src/assets/hatch_leg.png';
+import CFClogo from '../data/CFC_logo.png';
+import UD4Hlogo from '../data/logo2.png';
+import RedHatch from '../data/red_thin.png';
+import HatchLeg from '../data/hatch_leg.png';
 
 // load geojson
 //import METRICS from '../data/Metrics_for_website_4326.geojson?raw';
