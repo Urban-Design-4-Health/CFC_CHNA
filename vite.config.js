1b13328b39c6dfc776c86e9fff027aade76ae6be
William Bachman <billy@hometownlabs.io>
2025-03-30 01:37:21 -0400

    BB2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+//import resolve from '@rollup/plugin-node-resolve';
+//import commonjs from '@rollup/plugin-commonjs';
 
 
 // https://vite.dev/config/
